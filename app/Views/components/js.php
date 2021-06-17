e8c6938bb8d3ebe7f447dde1e7e526699cf1d7a3
nasirin50 <34644046+nasirin@users.noreply.github.com>
2021-06-17 11:45:18 +0700

    Initial commit

---
+++ app/Views/components/js.php
@@ -0,0 +1,40 @@
+<!-- jQuery -->
+<script src="../vendors/jquery/dist/jquery.min.js"></script>
+<!-- Bootstrap -->
+<script src="../vendors/bootstrap/dist/js/bootstrap.min.js"></script>
+<!-- FastClick -->
+<script src="../vendors/fastclick/lib/fastclick.js"></script>
+<!-- NProgress -->
+<script src="../vendors/nprogress/nprogress.js"></script>
+<!-- Chart.js -->
+<script src="/template/vendors/Chart.js/dist/Chart.min.js"></script>
+<!-- gauge.js -->
+<script src="/template/vendors/gauge.js/dist/gauge.min.js"></script>
+<!-- bootstrap-progressbar -->
+<script src="/template/vendors/bootstrap-progressbar/bootstrap-progressbar.min.js"></script>
+<!-- iCheck -->
+<script src="/template/vendors/iCheck/icheck.min.js"></script>
+<!-- Skycons -->
+<script src="/template/vendors/skycons/skycons.js"></script>
+<!-- Flot -->
+<script src="/template/vendors/Flot/jquery.flot.js"></script>
+<script src="/template/vendors/Flot/jquery.flot.pie.js"></script>
+<script src="/template/vendors/Flot/jquery.flot.time.js"></script>
+<script src="/template/vendors/Flot/jquery.flot.stack.js"></script>
+<script src="/template/vendors/Flot/jquery.flot.resize.js"></script>
+<!-- Flot plugins -->
+<script src="/template/vendors/flot.orderbars/js/jquery.flot.orderBars.js"></script>
+<script src="/template/vendors/flot-spline/js/jquery.flot.spline.min.js"></script>
+<script src="/template/vendors/flot.curvedlines/curvedLines.js"></script>
+<!-- DateJS -->
+<script src="/template/vendors/DateJS/build/date.js"></script>
+<!-- JQVMap -->
+<script src="/template/vendors/jqvmap/dist/jquery.vmap.js"></script>
+<script src="/template/vendors/jqvmap/dist/maps/jquery.vmap.world.js"></script>
+<script src="/template/vendors/jqvmap/examples/js/jquery.vmap.sampledata.js"></script>
+<!-- bootstrap-daterangepicker -->
+<script src="/template/vendors/moment/min/moment.min.js"></script>
+<script src="/template/vendors/bootstrap-daterangepicker/daterangepicker.js"></script>
+
+<!-- Custom Theme Scripts -->
+<script src="/template/build/js/custom.min.js"></script>
---
+++ app/Views/layouts/default.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?= $this->include('components/meta') ?>
+</head>
+
+<body class="nav-md">
+    <div class="container body">
+        <div class="main_container">
+            <?= $this->include('components/menu') ?>
+
+            <!-- top navigation -->
+            <?= $this->include('components/navbar') ?>
+            <!-- /top navigation -->
+
+            <!-- page content -->
+            <?= $this->renderSection('content') ?>
+            <!-- /page content -->
+        </div>
+    </div>
+
+    <?= $this->include('components/js') ?>
+
+</body>
+
+</html>
